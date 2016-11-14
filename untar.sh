82dfe26eb3b2f9244712d19007f5908a304f5eeb
Dammmy Kolawole <dcodesmith@gmail.com>
2016-11-14 20:46:29 +0000

    Added line to load nvm

--- untar.sh
+++ untar.sh
@@ -5,6 +5,9 @@ set -x
 # Perhaps set node version via nvm first. Match node versions on every environment
 # Rollback strategy to any given commit. Perhaps by commit number
 
+
+export NVM_DIR=$HOME/.nvm
+[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
 nvm install 6
 
 export NODE_ENV=production
