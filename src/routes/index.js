01abfdd86d53967f9cbd15db44267d59d4bcbaac
Dammmy Kolawole <dcodesmith@gmail.com>
2016-12-03 20:41:40 +0000

    Added system check route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import systemRoutes from './system';
 import channelRoutes from './channels';
 import programmeRoutes from './programmes';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+router.use('/status', systemRoutes);
 router.use('/channels', channelRoutes);
 router.use('/programmes', programmeRoutes);
 router.use((req, res, next) => {
--- src/routes/system.js
+++ src/routes/system.js
@@ -1,6 +1,10 @@
-module.exports = (router) => {
-  router
-    .head('/status', (req, res) => {
-      res.json({ status: 'ok' });
-    });
-};
+import express from 'express';
+
+const router = express.Router(); // eslint-disable-line new-cap
+
+router
+  .get('/', (req, res) => {
+    res.json({ status: 'ok' })
+  });
+
+export default router;
