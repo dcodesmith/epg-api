67f2a883fd2266a29f97c0ef7bb23aa35aa3c0c2
Dammmy Kolawole <dcodesmith@gmail.com>
2016-12-27 23:20:55 +0700

    refactor

--- untar.sh
+++ untar.sh
@@ -5,7 +5,6 @@ set -x
 # Perhaps set node version via nvm first. Match node versions on every environment
 # Rollback strategy to any given commit. Perhaps by commit number
 
-
 export NVM_DIR=$HOME/.nvm
 [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
 
@@ -15,16 +14,12 @@ nvm use 6.9.1
 export NODE_ENV=production
 export NVM_BIN=$HOME/.nvm/versions/node/v6.9.1/bin
 
-# Check node environment
-echo $NODE_ENV
-
 cd /srv/www/node/app && \
 tar zxvf package.tgz -C . && \
 cp -rf build/* . && \
-rm -rf build && \
-rm -rf utils index.js && \
-cp -rf dist/* . && rm -rf dist && \
-rm package.tgz && \
+rm -rf package.tgz build utils index.js && \
+cp -rf dist/* . && \
+rm -rf dist && \
 
 # Set production env variables
 export env $(cat config/.production.env)
