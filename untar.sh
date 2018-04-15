de29a4700c03fa962ecdffe667377662b6fc129b
Afees D Kolawole <dcodesmith@gmail.com>
2018-04-15 20:02:38 +0100

    Annotate untaring steps

--- untar.sh
+++ untar.sh
@@ -8,17 +8,23 @@ set -x
 export NVM_DIR=$HOME/.nvm
 [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
 
-nvm install 6
-nvm use 6.9.1
+nvm install 8
+nvm use 8.9.4
 
 export NODE_ENV=production
-export NVM_BIN=$HOME/.nvm/versions/node/v6.9.1/bin
+export NVM_BIN=$HOME/.nvm/versions/node/v8.9.4/bin
 
+echo 'changing directory to /srv/www/node/app ...'
 cd /srv/www/node/app && \
+echo 'unziping package.tgz into current directory ...'
 tar zxvf package.tgz -C . && \
+echo 'copying contents of build directory into current directory ...'
 cp -rf build/* . && \
+echo 'removing package.tgz build utils index.js from current directory ...'
 rm -rf package.tgz build utils index.js && \
+echo 'copying contents of dist directory into current directory ...'
 cp -rf dist/* . && \
+echo 'removing dist directory from current directory ...'
 rm -rf dist && \
 
 # Set production env variables
