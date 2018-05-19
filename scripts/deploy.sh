c8f11ecf2a102d9c00f05d2100f3cec7d7d2d38d
Afees D Kolawole <dcodesmith@gmail.com>
2018-05-19 11:12:24 +0100

    Debugging - Attempt to append production.env with sucure tokens

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,11 @@
 set -x
 
 mkdir build && \
+
+cat "$DATABASEPASSWORD" >> config/.production.env
+cat "$LOGGLYTOKEN" >> config/.production.env
+
+cat config/.production.env
 cp -rf dist app.yml config package.json build && \
 tar -czf package.tgz build && \
 scp package.tgz $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_DIR && \
